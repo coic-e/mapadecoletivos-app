861aa44b0d22ab00bd4bac7b513edd94fea28565
lucaoskaique <lucas.kaique175@hotmail.com>
2023-07-12 23:35:09 -0300

    Refactored code

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useHistory } from "react-router-dom";
+import { FiArrowLeft } from "react-icons/fi";
+
+import iconDiscoBall from "../../images/discoball-pequeno.svg";
+
+// import Discoball from "./Discoball";
+
+import './sidebar.css'
+
+function Sidebar(){
+  const history = useHistory();
+
+  const handleGoBack = () => {
+      if (history.length > 2) {
+          history.goBack();
+      } else {
+          history.push("/defaultpath"); //replace /defaultpath with your actual default route
+      }
+  }
+
+    return (        
+        <aside className="app-sidebar">
+        <div className="group-icons"></div>
+        <img src={iconDiscoBall} alt="Collective" />
+
+        <footer>
+          <button type="button" onClick={handleGoBack}>
+            <FiArrowLeft size={24} color="#FFF" />
+          </button>
+        </footer>
+      </aside>
+    );
+}
+
+export default Sidebar;
