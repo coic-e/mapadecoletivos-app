a2f76e4d6ecd3957e627bc95a5a09795f0f545f3
lucaoskaique <lucas.kaique175@hotmail.com>
2023-08-30 12:23:50 -0300

    fix identation

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import { useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 
@@ -6,31 +6,31 @@ import iconDiscoBall from "../../images/discoball-pequeno.svg";
 
 // import Discoball from "./Discoball";
 
-import './sidebar.css'
+import "./sidebar.css";
 
-function Sidebar(){
-  const history = useHistory();
+function Sidebar() {
+  const { goBack } = useHistory();
 
-  const handleGoBack = () => {
-      if (history.length > 2) {
-          history.goBack();
-      } else {
-          history.push("/defaultpath"); //replace /defaultpath with your actual default route
-      }
-  }
+  // const handleGoBack = () => {
+  //   if (history.length > 2) {
+  //     history.goBack();
+  //   } else {
+  //     history.push("/defaultpath"); //replace /defaultpath with your actual default route
+  //   }
+  // };
 
-    return (        
-        <aside className="app-sidebar">
-        <div className="group-icons"></div>
-        <img src={iconDiscoBall} alt="Collective" />
+  return (
+    <aside className="app-sidebar">
+      <div className="group-icons"></div>
+      <img src={iconDiscoBall} alt="Collective" />
 
-        <footer>
-          <button type="button" onClick={handleGoBack}>
-            <FiArrowLeft size={24} color="#FFF" />
-          </button>
-        </footer>
-      </aside>
-    );
+      <footer>
+        <button type="button" onClick={goBack}>
+          <FiArrowLeft size={24} color="#FFF" />
+        </button>
+      </footer>
+    </aside>
+  );
 }
 
 export default Sidebar;
