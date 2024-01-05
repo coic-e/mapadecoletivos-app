2b1aedbd50ba40b5edba5865bb5a14f8a799138a
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2024-01-05 14:37:12 -0300

    formData interface

---
+++ src/interfaces/FormData.ts
@@ -0,0 +1,12 @@
+export interface FormData {
+  name: string;
+  about: string;
+  email: string;
+  uf: string;
+  city: string;
+  type: string;
+  latitude: string;
+  longitude: string;
+  social: string;
+  images?: File[];
+}
